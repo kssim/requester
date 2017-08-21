5da90f8d2eaa974c12c9cdb72188c12556395b15
KyeongSeob Sim <ksub0912@gmail.com>
2017-08-21 18:03:07 +0900

    Implementation setup py. (not tested)

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup
+
+pwd = os.path.abspath(os.path.dirname(__file__))
+
+packages = ["requester"]
+
+requires = [
+    "requests>=2.4.3",
+    "ipaddress>=1.0.18"
+]
+
+with open("README.rst", "r", "utf-8") as f:
+    readme = f.read()
+
+setup(
+    name = "requester",
+    version = "0.5.0",
+    description = "Python HTTP/HTTPS Requester.",
+    long_description = "",
+    author = "kssim",
+    author_email = "ksub0912@gmail.com",
+    url = "https://github.com/kssim/requester",
+    packages = packages,
+    packages_data = {},
+    package_dir = {}
+    include_package_data = True,
+    install_requires = requires,
+    license = "MIT",
+    zip_safe = False,
+    classifiers = (
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Testing"
+    ),
+)
